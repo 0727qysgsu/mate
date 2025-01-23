473d69ff64c3f1bf8e998f709c3966c18199d3b7
0727qysgsu <117505382+0727qysgsu@users.noreply.github.com>
2025-01-23 14:15:57 +0700

    Delete score.php

--- score.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $score = intval($_POST['score']);
-    $_SESSION['high_score'] = max($_SESSION['high_score'] ?? 0, $score);
-    echo json_encode(['high_score' => $_SESSION['high_score']]);
-    exit;
-}
-?>
